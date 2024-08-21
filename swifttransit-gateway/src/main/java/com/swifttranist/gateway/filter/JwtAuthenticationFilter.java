3582e80a042043ea447c2ad6b8a744f68a0aff12
aix <aixlau@outlook.com>
2024-08-21 21:43:55 +0800

    feat: 修改 JWT 的 Access 和 Refresh Token 的生成与校验规则
    - 调整了 JWT 的生成逻辑，优化了 Token 的安全性。
    - 在 Gateway 中引入了白名单校验机制，通过 Redis 检查 Refresh Token 的有效性。
    - 优化了 Access Token 的验证逻辑，确保在 Token 有效时进一步检查白名单。

--- swifttransit-gateway/src/main/java/com/swifttranist/gateway/filter/JwtAuthenticationFilter.java
+++ swifttransit-gateway/src/main/java/com/swifttranist/gateway/filter/JwtAuthenticationFilter.java
@@ -1,13 +1,19 @@
 package com.swifttranist.gateway.filter;
 
+import com.aix.swifttransit.common.core.constant.CommonConstants;
 import com.aix.swifttransit.common.core.util.JwtUtil;
+import io.jsonwebtoken.MalformedJwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.server.ServerWebExchange;
@@ -18,7 +24,11 @@ import reactor.core.publisher.Mono;
 import java.util.Collections;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter implements WebFilter {
+
+    private final ReactiveRedisTemplate<String, String> redisTemplate;
+
     private static SecurityContext getSecurityContext(String username) {
         UserDetails user = new User(username, "", Collections.emptyList());
         UsernamePasswordAuthenticationToken authentication =
@@ -37,11 +47,28 @@ public class JwtAuthenticationFilter implements WebFilter {
         String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
         if (!ObjectUtils.isEmpty(authHeader) && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7);
-            if (JwtUtil.isTokenExpired(token)) {
+            boolean validateAccessToken;
+            try {
+                validateAccessToken = JwtUtil.validateAccessToken(token);
+            } catch (JwtException | IllegalArgumentException | MalformedJwtException e) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+            if (validateAccessToken) {
                 String username = JwtUtil.getUsername(token);
-                SecurityContext context = getSecurityContext(username);
-                return chain.filter(exchange)
-                        .contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(context)));
+                return redisTemplate.opsForValue().get(CommonConstants.REFRESH_TOKEN_KEY + username)
+                        .flatMap(refreshToken -> {
+                            if (ObjectUtils.isEmpty(refreshToken)) {
+                                // Redis 中不存在 RefreshToken，返回401
+                                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                                return exchange.getResponse().setComplete();
+                            } else {
+                                SecurityContext context = getSecurityContext(username);
+                                return chain.filter(exchange)
+                                        .contextWrite(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(context)));
+
+                            }
+                        });
             }
         }
         return chain.filter(exchange);
